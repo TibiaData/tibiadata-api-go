c382d840952df4da7bb6e0c37dab71117ae69179
Tobias Lindberg <tobias.ehlert@gmail.com>
2022-01-05 16:38:48 +0100

    adding support for house details view

--- src/TibiaHousesHouseV3.go
+++ src/TibiaHousesHouseV3.go
@@ -1,28 +1,183 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gin-gonic/gin"
+)
 
 // TibiaHousesHouseV3 func
 func TibiaHousesHouseV3(c *gin.Context) {
 
-	/*
-		// getting params from URL
-		world := c.Param("world")
-		houseid := c.Param("houseid")
-	*/
+	// getting params from URL
+	world := c.Param("world")
+	houseid := c.Param("houseid")
+
+	// Child of Status
+	type Rental struct {
+		Owner            string `json:"owner"`
+		OwnerSex         string `json:"owner_sex"`
+		PaidUntil        string `json:"paid_until"`
+		MovingDate       string `json:"moving_date"`
+		TransferReceiver string `json:"transfer_receiver"`
+		TransferPrice    int    `json:"transfer_price"`
+		TransferAccept   bool   `json:"transfer_accept"`
+	}
+
+	// Child of Status
+	type Auction struct {
+		CurrentBid     int    `json:"current_bid"`
+		CurrentBidder  string `json:"current_bidder"`
+		AuctionOngoing bool   `json:"auction_ongoing"`
+		AuctionEnd     string `json:"auction_end"`
+	}
+
+	// Child of House
+	type Status struct {
+		IsRented      bool    `json:"is_rented"`
+		IsAuctioned   bool    `json:"is_auctioned"`
+		IsTransfering bool    `json:"is_transfering"`
+		IsMoving      bool    `json:"is_moving"`
+		TheRental     Rental  `json:"rental"`
+		TheAuction    Auction `json:"auction"`
+		Original      string  `json:"original"`
+	}
+
+	// Child of JSONData
+	type House struct {
+		Houseid int    `json:"houseid"`
+		World   string `json:"world"`
+		Town    string `json:"town"`
+		Name    string `json:"name"`
+		Type    string `json:"type"`
+		Beds    int    `json:"beds"`
+		Size    int    `json:"size"`
+		Rent    int    `json:"rent"`
+		Img     string `json:"img"`
+		Status  Status `json:"status"`
+	}
 
 	//
-	// The base
-	type JSONData struct{}
+	// The base includes two levels: Houses and Information
+	type JSONData struct {
+		House       House       `json:"house"`
+		Information Information `json:"information"`
+	}
 
-	/*
-		// Adding fix for First letter to be upper and rest lower
-		world = TibiadataStringWorldFormatToTitleV3(world)
-	*/
+	// Creating empty vars
+	var HouseData House
+
+	// Adding fix for First letter to be upper and rest lower
+	world = TibiadataStringWorldFormatToTitleV3(world)
+
+	// Getting data with TibiadataHTMLDataCollectorV3
+	BoxContentHTML := TibiadataHTMLDataCollectorV3("https://www.tibia.com/community/?subtopic=houses&page=view&world=" + TibiadataQueryEscapeStringV3(world) + "&houseid=" + TibiadataQueryEscapeStringV3(houseid))
+
+	// Loading HTML data into ReaderHTML for goquery with NewReader
+	ReaderHTML, err := goquery.NewDocumentFromReader(strings.NewReader(BoxContentHTML))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Running query over each div
+	HouseHTML, err := ReaderHTML.Find(".BoxContent table tr").First().Html()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Regex to get data for house
+	regex1 := regexp.MustCompile(`<td.*src="(.*)" width.*<b>(.*)<\/b>.*This ([A-Za-z]+) can.*to ([0-9]+) beds..*<b>([0-9]+) square.*<b>([0-9]+)([k]+).gold<\/b>.*on <b>([A-Za-z]+)<\/b>.(.*)<\/td>`)
+	subma1 := regex1.FindAllStringSubmatch(HouseHTML, -1)
+
+	if len(subma1) > 0 {
+		HouseData.Houseid = TibiadataStringToIntegerV3(houseid)
+		HouseData.World = subma1[0][8]
+
+		HouseData.Name = TibiaDataSanitizeEscapedString(subma1[0][2])
+		HouseData.Img = subma1[0][1]
+		HouseData.Type = subma1[0][3]
+		HouseData.Beds = TibiadataStringToIntegerV3(subma1[0][4])
+		HouseData.Size = TibiadataStringToIntegerV3(subma1[0][5])
+		HouseData.Rent = TibiadataStringToIntegerV3(subma1[0][6] + strings.Repeat("000", strings.Count(subma1[0][7], "k")))
+
+		HouseData.Status.Original = TibiaDataSanitizeEscapedString(RemoveHtmlTag(subma1[0][9]))
+
+		switch {
+		case strings.Contains(HouseData.Status.Original, "has been rented by"):
+			// rented
+
+			switch {
+			case strings.Contains(HouseData.Status.Original, " pass the "+HouseData.Type+" to "):
+				HouseData.Status.IsTransfering = true
+				// matching for this: and <wants to|will> pass the <HouseType> to <TransferReceiver> for <TransferPrice> gold
+				regex2 := regexp.MustCompile(`and (wants to|will) pass the (house|guildhall) to (.*) for ([0-9]+) gold`)
+				subma2 := regex2.FindAllStringSubmatch(HouseData.Status.Original, -1)
+				// storing values from regex
+				if subma2[0][1] == "will" {
+					HouseData.Status.TheRental.TransferAccept = true
+				}
+				HouseData.Status.TheRental.TransferReceiver = subma2[0][3]
+				HouseData.Status.TheRental.TransferPrice = TibiadataStringToIntegerV3(subma2[0][4])
+				fallthrough
+
+			case strings.Contains(HouseData.Status.Original, " will move out on "):
+				HouseData.Status.IsMoving = true
+				// matching for this: <OwnerSex> will move out on <MovingDate> (
+				regex2 := regexp.MustCompile(`(He|She) will move out on (.*?) \(`)
+				subma2 := regex2.FindAllStringSubmatch(HouseData.Status.Original, -1)
+				// storing values from regex
+				HouseData.Status.TheRental.MovingDate = TibiadataDatetimeV3(subma2[0][2])
+				fallthrough
+
+			default:
+				HouseData.Status.IsRented = true
+				// matching for this: The <HouseType> has been rented by <Owner>. <OwnerSex> has paid the rent until <PaidUntil>.
+				regex2 := regexp.MustCompile(`The (house|guildhall) has been rented by (.*). (He|She) has paid.*until (.*?)\.`)
+				subma2 := regex2.FindAllStringSubmatch(HouseData.Status.Original, -1)
+				// storing values from regex
+				HouseData.Status.TheRental.Owner = subma2[0][2]
+				HouseData.Status.TheRental.PaidUntil = TibiadataDatetimeV3(subma2[0][4])
+				switch subma2[0][3] {
+				case "She":
+					HouseData.Status.TheRental.OwnerSex = "female"
+				case "He":
+					HouseData.Status.TheRental.OwnerSex = "male"
+				}
+			}
+
+		case strings.Contains(HouseData.Status.Original, "is currently being auctioned"):
+			// auctioned
+			HouseData.Status.IsAuctioned = true
+
+			// check if bid is going on
+			if !strings.Contains(HouseData.Status.Original, "No bid has been submitted so far.") {
+				regex2 := regexp.MustCompile(`The (house|guildhall) is currently.*The auction (will end|has ended) at (.*)\. The.*is ([0-9]+) gold.*submitted by (.*)\.`)
+				subma2 := regex2.FindAllStringSubmatch(HouseData.Status.Original, -1)
+				// storing values from regex
+				HouseData.Status.TheAuction.AuctionEnd = TibiadataDatetimeV3(subma2[0][3])
+				HouseData.Status.TheAuction.CurrentBid = TibiadataStringToIntegerV3(subma2[0][4])
+				HouseData.Status.TheAuction.CurrentBidder = subma2[0][5]
+				if subma2[0][2] == "will end" {
+					HouseData.Status.TheAuction.AuctionOngoing = true
+				}
+			}
+		}
+
+	}
 
 	//
 	// Build the data-blob
-	jsonData := JSONData{}
+	jsonData := JSONData{
+		HouseData,
+		Information{
+			APIVersion: TibiadataAPIversion,
+			Timestamp:  TibiadataDatetimeV3(""),
+		},
+	}
 
 	// return jsonData
 	TibiaDataAPIHandleSuccessResponse(c, "TibiaHousesHouseV3", jsonData)

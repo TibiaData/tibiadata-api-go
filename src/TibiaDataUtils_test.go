4e68c81d5bf3403c62c6e91f3f95841ba8a47e2d
Kennedy Bushnell <kennedybushnell@kamilon.com>
2022-01-08 21:52:31 -0800

    Handle timezone information during DateTime parsing

---
+++ src/TibiaDataUtils_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTibiaCETDateFormat(t *testing.T) {
+	assert.Equal(t, "2021-12-24T09:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 CET"))
+}
+
+func TestTibiaCESTDateFormat(t *testing.T) {
+	assert.Equal(t, "2021-12-24T09:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 CEST"))
+}
+
+func TestTibiaUTCDateFormat(t *testing.T) {
+	assert.Equal(t, "2021-12-24T09:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 UTC"))
+}
+
+func TestTibiaPSTDateFormat(t *testing.T) {
+	assert.Equal(t, "2021-12-24T17:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 PST"))
+}

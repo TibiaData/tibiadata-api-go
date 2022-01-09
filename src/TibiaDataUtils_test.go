1407e13f91fce600fefe9415cfb3454058f35d52
Tobias Lindberg <tobias.ehlert@gmail.com>
2022-01-10 00:26:22 +0100

    removing not used test for EST/PST timezones

--- src/TibiaDataUtils_test.go
+++ src/TibiaDataUtils_test.go
@@ -17,13 +17,3 @@ func TestTibiaCESTDateFormat(t *testing.T) {
 func TestTibiaUTCDateFormat(t *testing.T) {
 	assert.Equal(t, "2021-12-24T09:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 UTC"))
 }
-
-/*
-func TestTibiaESTDateFormat(t *testing.T) {
-	assert.Equal(t, "2021-12-24T14:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 EST"))
-}
-
-func TestTibiaPSTDateFormat(t *testing.T) {
-	assert.Equal(t, "2021-12-24T17:52:16Z", TibiadataDatetimeV3("Dec 24 2021, 09:52:16 PST"))
-}
-*/

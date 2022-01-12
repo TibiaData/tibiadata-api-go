39c0a6d26a413e03f37f3b30dc5ee34899da5ca9
kamilon <kennedybushnell@kamilon.com>
2022-01-12 13:05:51 -0800

    Add unit tests to Kill Statistics API, move to goquery (#57)

--- src/TibiaKillstatisticsV3.go
+++ src/TibiaKillstatisticsV3.go
@@ -3,53 +3,47 @@ package main
 import (
 	"log"
 	"net/http"
-	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	KillStatisticInformationRegex = regexp.MustCompile(`<td>(.*)<\/td><td.*>([0-9]+).*<\/td><td.*>([0-9]+).*<\/td><td.*>([0-9]+).*<\/td><td.*>([0-9]+).*<\/td>`)
-)
+// Child of KillStatistics
+type Entry struct {
+	Race                    string `json:"race"`
+	LastDayKilledPlayers    int    `json:"last_day_players_killed"`
+	LastDayKilledByPlayers  int    `json:"last_day_killed"`
+	LastWeekKilledPlayers   int    `json:"last_week_players_killed"`
+	LastWeekKilledByPlayers int    `json:"last_week_killed"`
+}
+
+// Child of KillStatistics
+type Total struct {
+	LastDayKilledPlayers    int `json:"last_day_players_killed"`
+	LastDayKilledByPlayers  int `json:"last_day_killed"`
+	LastWeekKilledPlayers   int `json:"last_week_players_killed"`
+	LastWeekKilledByPlayers int `json:"last_week_killed"`
+}
+
+// Child of JSONData
+type KillStatistics struct {
+	World   string  `json:"world"`
+	Entries []Entry `json:"entries"`
+	Total   Total   `json:"total"`
+}
+
+//
+// The base includes two levels: KillStatistics and Information
+type KillStatisticsResponse struct {
+	KillStatistics KillStatistics `json:"killstatistics"`
+	Information    Information    `json:"information"`
+}
 
 // TibiaKillstatisticsV3 func
 func TibiaKillstatisticsV3(c *gin.Context) {
-
 	// getting params from URL
 	world := c.Param("world")
-
-	// Child of KillStatistics
-	type Entry struct {
-		Race                    string `json:"race"`
-		LastDayKilledPlayers    int    `json:"last_day_players_killed"`
-		LastDayKilledByPlayers  int    `json:"last_day_killed"`
-		LastWeekKilledPlayers   int    `json:"last_week_players_killed"`
-		LastWeekKilledByPlayers int    `json:"last_week_killed"`
-	}
-
-	// Child of KillStatistics
-	type Total struct {
-		LastDayKilledPlayers    int `json:"last_day_players_killed"`
-		LastDayKilledByPlayers  int `json:"last_day_killed"`
-		LastWeekKilledPlayers   int `json:"last_week_players_killed"`
-		LastWeekKilledByPlayers int `json:"last_week_killed"`
-	}
-
-	// Child of JSONData
-	type KillStatistics struct {
-		World   string  `json:"world"`
-		Entries []Entry `json:"entries"`
-		Total   Total   `json:"total"`
-	}
-
-	//
-	// The base includes two levels: KillStatistics and Information
-	type JSONData struct {
-		KillStatistics KillStatistics `json:"killstatistics"`
-		Information    Information    `json:"information"`
-	}
 
 	// Adding fix for First letter to be upper and rest lower
 	world = TibiadataStringWorldFormatToTitleV3(world)
@@ -64,6 +58,13 @@ func TibiaKillstatisticsV3(c *gin.Context) {
 		return
 	}
 
+	jsonData := TibiaKillstatisticsV3Impl(world, BoxContentHTML)
+
+	// return jsonData
+	TibiaDataAPIHandleSuccessResponse(c, "TibiaKillstatisticsV3", jsonData)
+}
+
+func TibiaKillstatisticsV3Impl(world string, BoxContentHTML string) KillStatisticsResponse {
 	// Loading HTML data into ReaderHTML for goquery with NewReader
 	ReaderHTML, err := goquery.NewDocumentFromReader(strings.NewReader(BoxContentHTML))
 	if err != nil {
@@ -77,47 +78,31 @@ func TibiaKillstatisticsV3(c *gin.Context) {
 	)
 
 	// Running query over each div
-	ReaderHTML.Find("#KillStatisticsTable .TableContent tr").Each(func(index int, s *goquery.Selection) {
+	ReaderHTML.Find("#KillStatisticsTable .TableContent tr.Odd,tr.Even").Each(func(index int, s *goquery.Selection) {
+		DataColumns := s.Find("td").Nodes
 
-		// Storing HTML into CreatureDivHTML
-		KillStatisticsDivHTML, err := s.Html()
-		if err != nil {
-			log.Fatal(err)
-		}
+		KillStatisticsLastDayKilledPlayers := TibiadataStringToIntegerV3(DataColumns[1].FirstChild.Data)
+		TotalLastDayKilledPlayers += KillStatisticsLastDayKilledPlayers
+		KillStatisticsLastDayKilledByPlayers := TibiadataStringToIntegerV3(DataColumns[2].FirstChild.Data)
+		TotalLastDayKilledByPlayers += KillStatisticsLastDayKilledByPlayers
+		KillStatisticsLastWeekKilledPlayers := TibiadataStringToIntegerV3(DataColumns[3].FirstChild.Data)
+		TotalLastWeekKilledPlayers += KillStatisticsLastWeekKilledPlayers
+		KillStatisticsLastWeekKilledByPlayers := TibiadataStringToIntegerV3(DataColumns[4].FirstChild.Data)
+		TotalLastWeekKilledByPlayers += KillStatisticsLastWeekKilledByPlayers
 
-		subma1 := KillStatisticInformationRegex.FindAllStringSubmatch(KillStatisticsDivHTML, -1)
-
-		if len(subma1) > 0 {
-
-			if strings.TrimSpace(subma1[0][1]) == "Total" {
-				// we don't want to include the Total row
-			} else {
-
-				// Store the values..
-				KillStatisticsLastDayKilledPlayers := TibiadataStringToIntegerV3(subma1[0][2])
-				TotalLastDayKilledPlayers += KillStatisticsLastDayKilledPlayers
-				KillStatisticsLastDayKilledByPlayers := TibiadataStringToIntegerV3(subma1[0][3])
-				TotalLastDayKilledByPlayers += KillStatisticsLastDayKilledByPlayers
-				KillStatisticsLastWeekKilledPlayers := TibiadataStringToIntegerV3(subma1[0][4])
-				TotalLastWeekKilledPlayers += KillStatisticsLastWeekKilledPlayers
-				KillStatisticsLastWeekKilledByPlayers := TibiadataStringToIntegerV3(subma1[0][5])
-				TotalLastWeekKilledByPlayers += KillStatisticsLastWeekKilledByPlayers
-
-				// Append new Entry item to KillStatisticsData
-				KillStatisticsData = append(KillStatisticsData, Entry{
-					Race:                    TibiaDataSanitizeEscapedString(subma1[0][1]),
-					LastDayKilledPlayers:    KillStatisticsLastDayKilledPlayers,
-					LastDayKilledByPlayers:  KillStatisticsLastDayKilledByPlayers,
-					LastWeekKilledPlayers:   KillStatisticsLastWeekKilledPlayers,
-					LastWeekKilledByPlayers: KillStatisticsLastWeekKilledByPlayers,
-				})
-			}
-		}
+		// Append new Entry item to KillStatisticsData
+		KillStatisticsData = append(KillStatisticsData, Entry{
+			Race:                    TibiaDataSanitizeEscapedString(DataColumns[0].FirstChild.Data),
+			LastDayKilledPlayers:    KillStatisticsLastDayKilledPlayers,
+			LastDayKilledByPlayers:  KillStatisticsLastDayKilledByPlayers,
+			LastWeekKilledPlayers:   KillStatisticsLastWeekKilledPlayers,
+			LastWeekKilledByPlayers: KillStatisticsLastWeekKilledByPlayers,
+		})
 	})
 
 	//
 	// Build the data-blob
-	jsonData := JSONData{
+	return KillStatisticsResponse{
 		KillStatistics{
 			World:   world,
 			Entries: KillStatisticsData,
@@ -133,8 +118,4 @@ func TibiaKillstatisticsV3(c *gin.Context) {
 			Timestamp:  TibiadataDatetimeV3(""),
 		},
 	}
-
-	// return jsonData
-	TibiaDataAPIHandleSuccessResponse(c, "TibiaKillstatisticsV3", jsonData)
-
 }
---
+++ src/TibiaKillstatisticsV3_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAntica(t *testing.T) {
+	data, err := ioutil.ReadFile("../testdata/killstatistics/Antica.html")
+	if err != nil {
+		t.Errorf("File reading error: %s", err)
+		return
+	}
+
+	anticaJson := TibiaKillstatisticsV3Impl("Antica", string(data))
+	assert := assert.New(t)
+
+	assert.Equal("Antica", anticaJson.KillStatistics.World)
+	assert.Equal(1159, len(anticaJson.KillStatistics.Entries))
+
+	elementalForces := anticaJson.KillStatistics.Entries[0]
+	assert.Equal("(elemental forces)", elementalForces.Race)
+	assert.Equal(6, elementalForces.LastDayKilledPlayers)
+	assert.Equal(0, elementalForces.LastDayKilledByPlayers)
+	assert.Equal(103, elementalForces.LastWeekKilledPlayers)
+	assert.Equal(0, elementalForces.LastWeekKilledByPlayers)
+
+	caveRats := anticaJson.KillStatistics.Entries[386]
+	assert.Equal("cave rats", caveRats.Race)
+	assert.Equal(2, caveRats.LastDayKilledPlayers)
+	assert.Equal(1618, caveRats.LastDayKilledByPlayers)
+	assert.Equal(78, caveRats.LastWeekKilledPlayers)
+	assert.Equal(12876, caveRats.LastWeekKilledByPlayers)
+}
+
+func BenchmarkAntica(b *testing.B) {
+	data, err := ioutil.ReadFile("../testdata/killstatistics/Antica.html")
+	if err != nil {
+		b.Errorf("File reading error: %s", err)
+		return
+	}
+
+	b.ReportAllocs()
+
+	assert := assert.New(b)
+
+	for i := 0; i < b.N; i++ {
+		anticaJson := TibiaKillstatisticsV3Impl("Antica", string(data))
+
+		assert.Equal("Antica", anticaJson.KillStatistics.World)
+	}
+}

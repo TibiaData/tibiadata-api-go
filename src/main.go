1b101b0e8443019e693a56997a34d2e1ac65e236
Tobias Lindberg <tobias.ehlert@gmail.com>
2022-01-18 21:57:49 +0100

    adjusting swagger info slightly

--- src/main.go
+++ src/main.go
@@ -22,17 +22,19 @@ var (
 
 // @title           TibiaData API
 // @version         edge
-// @description     This is the API documentation for the TibiaData API written in Golang.
+// @description     This is the API documentation for the TibiaData API.
+// @description     The documentation contains version 3 and above.
 // @termsOfService  https://tibiadata.com/terms/
 
-// @contact.name   TibiaData Support
-// @contact.url    https://tibiadata.com/contact/
+// @contact.name   TibiaData
+// @contact.url    https://www.tibiadata.com/contact/
 // @contact.email  tobias@tibiadata.com
 
 // @license.name  MIT
 // @license.url   https://github.com/TibiaData/tibiadata-api-go/blob/main/LICENSE
 
-// @host      dev.tibiadata.com
+// @schemes   http
+// @host      localhost:8080
 // @BasePath  /
 
 func main() {

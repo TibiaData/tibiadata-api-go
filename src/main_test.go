6ff3bb5735f216572097e9b336b7b719e9f9c6fb
Tobias Lindberg <tobias.ehlert@gmail.com>
2025-02-26 10:11:00 +0100

    fix: url_api field format on news endpoints

--- src/main_test.go
+++ src/main_test.go
@@ -7,19 +7,30 @@ import (
 )
 
 func TestInit(t *testing.T) {
-	const expectedUserAgent = "TibiaData-API/v4 (release/unknown; build/manual; commit/-; edition/open-source; unittest.example.com)"
+	const (
+		expectedUserAgent     = "TibiaData-API/v4 (release/unknown; build/manual; commit/-; edition/open-source)"
+		expectedUserAgentHost = "TibiaData-API/v4 (release/unknown; build/manual; commit/-; edition/open-source; +https://unittest.example.com)"
+	)
 
+	TibiaDataHost = ""
 	TibiaDataUserAgent = TibiaDataUserAgentGenerator(TibiaDataAPIversion)
 	assert.Equal(t, expectedUserAgent, TibiaDataUserAgent)
+
+	TibiaDataHost = "unittest.example.com"
+	TibiaDataUserAgent = TibiaDataUserAgentGenerator(TibiaDataAPIversion)
+	assert.Equal(t, expectedUserAgentHost, TibiaDataUserAgent)
 }
 
 func TestTibiaDataInitializer(t *testing.T) {
 	assert := assert.New(t)
+
+	TibiaDataHost = "unittest.example.com"
 
 	// Call the function to be tested
 	TibiaDataInitializer()
 
 	// Check that the variables have been set correctly
 	assert.Equal("open-source", TibiaDataBuildEdition)
+	assert.Equal("https", TibiaDataProtocol)
 	assert.Equal("unittest.example.com", TibiaDataHost)
 }
